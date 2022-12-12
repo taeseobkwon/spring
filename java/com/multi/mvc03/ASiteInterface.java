d8edc3cbc56752425d09428b8f99935904562481
Taeseob Kwon <113962988+taeseobkwon@users.noreply.github.com>
2022-12-12 23:44:44 +0900

    Delete java/com/multi/mvc03 directory

--- java/com/multi/mvc03/ASiteInterface.java
+++
@@ -1,7 +0,0 @@
-package com.multi.mvc03;
-
-public interface ASiteInterface {
-
-	void person();
-
-}
--- java/com/multi/mvc03/MemberDAOInterface.java
+++
@@ -1,9 +0,0 @@
-package com.multi.mvc03;
-
-public interface MemberDAOInterface {
-	void login();
-	void insert();
-	void delete();
-	void update();
-	
-}
